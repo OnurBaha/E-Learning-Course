d0557e6a2cd3f7cac079ee04c296b71650c288b0
OnurBaha <koconurbaha@gmail.com>
2024-11-20 18:53:49 +0300

    Pages and service added. Basic filing completed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CourseDetailsComponent } from './pages/course-details/course-details.component';
+import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full',
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'coursedetail',
+        component:CourseDetailsComponent
+    },
+    {
+        path:'my-course',
+        component:MyCoursesComponent
+    },
+];
