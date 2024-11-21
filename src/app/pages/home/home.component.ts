a2ab1666caa7ce25e3fe70bb0d907df3337b18e6
OnurBaha <koconurbaha@gmail.com>
2024-11-21 15:05:33 +0300

    Fetched and displayed courses using API service integration

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { IApiResponse, Icourse } from '../../model/master.model';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
+  masterSrv = inject(MasterService)
+  courseList = signal<Icourse[]>([])
+
+  ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  loadCourses(){
+    this.masterSrv.getAllCourse().subscribe((res:IApiResponse)=>{
+      this.courseList.set(res.data)
+    }, error=>{
+
+    })
+  }
 
 }
--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IApiResponse } from '../model/master.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MasterService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  apiUrl: string = "https://projectapi.gerasim.in/api/OnlineLearning/";
+
+  getAllCourse(): Observable<IApiResponse>{
+    return this.http.get<IApiResponse>(`${this.apiUrl}GetAllCourse`)
+  }
+
 }
