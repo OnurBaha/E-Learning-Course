fb506aa7e7b5b424cfef1a528d147c68723a604d
OnurBaha <koconurbaha@gmail.com>
2024-11-26 20:51:36 +0300

    Completed the implementation of fetching the next video in the course content.

--- src/app/pages/course-details/course-details.component.ts
+++ src/app/pages/course-details/course-details.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { IApiResponse, ICourseWithVideos, IEnrollmentCourse, User, Video } from '../../model/master.model';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-course-details',
@@ -9,4 +14,31 @@ import { Component } from '@angular/core';
 })
 export class CourseDetailsComponent {
 
+  masterSrv = inject(MasterService)
+  loggedUserData: User = new User();
+  videoList: Video[] = [];
+  router = inject(Router)
+
+  activatedRoute = inject(ActivatedRoute)
+  courseId:number=0
+
+  constructor() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const localData = localStorage.getItem('learningUser');
+      if (localData != null) {
+        const parsedData = JSON.parse(localData);
+        this.loggedUserData = parsedData;
+      }
+      this.activatedRoute.params.subscribe((res:any)=>{
+        this.courseId=res.id;
+        this.getVideos();
+      })
+    }
+  }
+
+  getVideos() {
+    this.masterSrv.getCourseVideosbyCourseId(this.courseId).subscribe((res: IApiResponse) => {
+      this.videoList = res.data;
+    })
+  }
 }
--- src/app/pages/my-courses/my-courses.component.ts
+++ src/app/pages/my-courses/my-courses.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, Inject, OnInit } from '@angular/core';
-import { IApiResponse, Icourse, IEnrollmentCourse, User } from '../../model/master.model';
+import { IApiResponse, Icourse, ICourseWithVideos, IEnrollmentCourse, User } from '../../model/master.model';
 import { MasterService } from '../../services/master.service';
 import { SlicePipe } from '@angular/common';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-courses',
@@ -11,35 +11,27 @@ import { Router } from '@angular/router';
   templateUrl: './my-courses.component.html',
   styleUrl: './my-courses.component.css'
 })
-export class MyCoursesComponent implements OnInit{
+export class MyCoursesComponent implements OnInit {
 
 
   masterSrv = inject(MasterService)
   loggedUserData: User = new User();
-  courseList:IEnrollmentCourse[] =[];
-  router = inject(Router) 
-  
-  constructor() {
-    if (typeof window !== 'undefined' && window.localStorage) {
-      const localData = localStorage.getItem('learningUser');
-      if (localData != null) {
-        const parsedData = JSON.parse(localData);
-        this.loggedUserData = parsedData;
-      }
-    }
-  }
+  courseList: IEnrollmentCourse[] = [];
+  router = inject(Router)
+
 
   ngOnInit(): void {
     this.getEnrollmentByUserId()
   }
-  
-  navigatingToCourse(id:number){
+
+  navigatingToCourse(id: number) {
     this.router.navigate(['coursedetail', id])
   }
 
-  getEnrollmentByUserId(){
-    this.masterSrv.getEnrolledCourseByUserId(this.loggedUserData.userId).subscribe((res:IApiResponse)=>{
+  getEnrollmentByUserId() {
+    this.masterSrv.getEnrolledCourseByUserId(this.loggedUserData.userId).subscribe((res: IApiResponse) => {
       this.courseList = res.data;
     })
   }
+
 }
